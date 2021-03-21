7825e9884cc01b19463447b015619eba77f056c4
D·Teach <1114626450@qq.com>
2021-03-21 22:55:52 +0800

    演示web-demo

---
+++ boot-05-web-01/src/main/java/com/dlq/boot/controller/HelloController.java
@@ -0,0 +1,41 @@
+package com.dlq.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *@program: SpringBoot2
+ *@description:
+ *@author: Hasee
+ *@create: 2021-01-22 21:16
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("static.jpg")
+    public String hello(){
+        return "aaaa";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    public String getUser(){
+        return "GET-张三";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.POST)
+    public String saveUser(){
+        return "POST-张三";
+    }
+
+
+    @RequestMapping(value = "/user",method = RequestMethod.PUT)
+    public String putUser(){
+        return "PUT-张三";
+    }
+
+    @RequestMapping(value = "/user/del",method = RequestMethod.DELETE)
+    public String deleteUser(){
+        return "DELETE-张三";
+    }
+}
