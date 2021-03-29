b681428e1015c09078f71211f64221f86e99aa11
D·Teach <1114626450@qq.com>
2021-03-29 23:16:28 +0800

    web源码案例

--- boot-05-web-01/src/main/java/com/dlq/boot/config/WebConfig.java
+++ boot-05-web-01/src/main/java/com/dlq/boot/config/WebConfig.java
@@ -1,15 +1,26 @@
 package com.dlq.boot.config;
 
 import com.dlq.boot.bean.Pet;
+import com.dlq.boot.converter.ZdyMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.util.StringUtils;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+import org.springframework.web.accept.ParameterContentNegotiationStrategy;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.UrlPathHelper;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  *@program: SpringBoot2
@@ -41,6 +52,27 @@ public class WebConfig /*implements WebMvcConfigurer*/ {
     @Bean
     public WebMvcConfigurer webMvcConfigurer() {
         return new WebMvcConfigurer() {
+
+            //自定义内容协商策略
+            @Override
+            public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+                //Map<String, MediaType> mediaTypes
+                Map<String, MediaType> mediaTypes = new HashMap<>();
+                mediaTypes.put("json", MediaType.APPLICATION_JSON);
+                mediaTypes.put("xml", MediaType.APPLICATION_XML);
+                mediaTypes.put("zdy", MediaType.parseMediaType("application/x-zdy"));
+                //指定支持解析哪些参数对应的哪些媒体类型
+                ParameterContentNegotiationStrategy parameterStrategy =
+                        new ParameterContentNegotiationStrategy(mediaTypes);
+                HeaderContentNegotiationStrategy headerStrategy = new HeaderContentNegotiationStrategy();
+                configurer.strategies(Arrays.asList(parameterStrategy,headerStrategy));
+            }
+
+            @Override
+            public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+                converters.add(new ZdyMessageConverter());
+            }
+
             @Override
             public void configurePathMatch(PathMatchConfigurer configurer) {
                 UrlPathHelper urlPathHelper = new UrlPathHelper();
